23fdb3dee33ab0c05f9b873f28009b1f605c07f6
bslattz <robert_slattery@hotmail.com>
2017-02-27 15:17:34 +1100

    Replaced xml builder with litteral

--- postAction.ps1
+++ postAction.ps1
@@ -5,22 +5,14 @@
 # Optionaly accepts version number (defaults to 1.0.0.0)
 # Adds a postActions Node to the current version of the application manifest
 # Uses pfx file in the current project
-# signs the application manifest
-# updates the deployment manifest
+#  signs the application manifest
+#  updates the deployment manifest trust
 # copy the .vsto file to the current version Application Files folder
 
 param(
     [string] $postActionClass,
     [string] $postActionVer = "1.0.0.0"
 )
-
-function appendAttr([xml]$doc, [Xml.XmlNode]$node, [string]$name, [string]$value) {
-    
-    $Attr = $doc.CreateAttribute($name)
-    $Attr.value = $value
-    $node.Attributes.Append($Attr)
-    return $node
-}
 
 $CWD = $PWD
 
@@ -50,39 +42,62 @@ $deploymentManifestFile = $publishFolder | Where-Object {$_.Extension -eq ".vsto
 
 [xml]$deploymentManifest = Get-Content $deploymentManifestFile.FullName
 
-# find the current application version
+# find the current application version and get the application manifest
 $CurrentApplicationManifestPath = Join-Path $publishPath $deploymentManifest.assembly.dependency.dependentAssembly.codebase
 [xml]$CurrentApplicationManifest = Get-Content -Path $CurrentApplicationManifestPath
 
 # build the postActions node and add it to the application m/fest after the </vstav3:update> element
 $doc = $CurrentApplicationManifest
-$vstav3NS = "urn:schemas-microsoft-com:vsta.v3"
-$postActionsNode = $doc.CreateElement("postActions", $vstav3NS)
-[Xml.XmlNode]$postAction = $postActionsNode.AppendChild($doc.CreateElement("postAction", $vstav3NS))
-[Xml.XmlNode]$entryPoint = $postAction.AppendChild($doc.CreateElement("entryPoint", $vstav3NS))
-[Xml.XmlNode]$assemblyIdentity = $entryPoint.AppendChild($doc.CreateElement("assemblyIdentity", $vstav3NS))
 
-appendAttr $doc $assemblyIdentity "name" $postActionClassName
-appendAttr $doc $assemblyIdentity "version" $postActionVer
-appendAttr $doc $assemblyIdentity "language" "neutral"
-appendAttr $doc $assemblyIdentity "processorArchitecture" "msil"
+[xml]$doc = @"
+<xml xmlns:vstav3="urn:schemas-microsoft-com:vsta.v3">
+    <vstav3:postActions>
+      <vstav3:postAction>
+        <vstav3:entryPoint
+          class="FileCopyPDA.FileCopyPDA">
+          <assemblyIdentity
+            name="FileCopyPDA"
+            version="1.0.0.0"
+            language="neutral"
+            processorArchitecture="msil" />
+        </vstav3:entryPoint>
+        <vstav3:postActionData>
+        </vstav3:postActionData>
+      </vstav3:postAction>
+    </vstav3:postActions>
+</xml>
+"@
+[Xml.XmlNode]$postActionsNode = $CurrentApplicationManifest.ImportNode($doc.xml.postActions, $true)
+[Xml.XmlNode]$entryPoint = $postActionsNode.postAction.entryPoint
+[Xml.XmlNode]$assemblyIdentity = $entryPoint.assemblyIdentity
 
-appendAttr $doc $entryPoint "class" $postActionClass
+$entryPoint.class = $postActionClass
+$assemblyIdentity.name = $postActionClassName
+$assemblyIdentity.version = $postActionVer
 
-$postActionsNode.postAction.AppendChild($doc.CreateElement("postActionData", $vstav3NS))
-
-Write-Debug $postActionsNode.OuterXml
+$sw=New-Object System.Io.Stringwriter
+$writer=New-Object System.Xml.XmlTextWriter($sw)
+$writer.Formatting = [System.Xml.Formatting]::Indented
+$doc.WriteContentTo($writer)
+$sw.ToString()
 
 # Replace any existing version
-$addin = $doc.assembly.addIn
-$prevPANode = $addin.GetElementsByTagName("postActions", $vstav3NS)[0]
+$addin = $CurrentApplicationManifest.assembly.addIn
+$prevPANode = $addin.postActions
 if($prevPANode -eq $null){
-    $addin.InsertAfter($postActionsNode, $doc.GetElementsByTagName("update", $vstav3NS))
+    $addin.InsertAfter($postActionsNode, $addin.update)
 }else{
     $addin.ReplaceChild($postActionsNode, $prevPANode)
 }
 
-$doc.Save($CurrentApplicationManifestPath)
+$CurrentApplicationManifest.Save($CurrentApplicationManifestPath)
+
+# Use the pfx file in the current project
 
 $Certificate = $ProjectFolder | Where-Object {$_.Extension -eq $certExtension}
 
+#  sign the application manifest
+mage -sign $CurrentApplicationManifestPath -certfile $Certificate.FullName
+
+#  update the deployment manifest trust
+mage -update $deploymentManifestFile.FullName  -appmanifest $CurrentApplicationManifestPath -certfile $Certificate.FullName
---
+++ test.ps1
@@ -0,0 +1,24 @@
+﻿param (
+    [string]$target = "../bash"
+)
+
+. .\Utils.ps1
+. .\GitUtils.ps1
+. .\GitPrompt.ps1
+
+$CDR = $PWD
+
+cd $target
+
+Write-Output $target
+
+$s = Get-GitStatus $target
+
+Write-Output ConvertTo-Json $s
+
+$s = Write-GitStatus $s
+
+Write-Output $s
+
+cd $CDR
+
